04de46e75e68a8978315465313201925f6051760
Bob Glickstein <bobg@users.noreply.github.com>
2025-06-08 11:41:11 -0700

    Add FirstUntil (#5)
    * Add FirstUntil, by analogy with SkipUntil.
    
    * Add SkipWhile and FirstWhile as convenience functions.

--- first.go
+++ first.go
@@ -2,6 +2,31 @@ package seqs
 
 import "iter"
 
+// FirstUntil copies the input iterator to the output
+// until the first element that causes f to return true.
+// It is the same as FirstWhile(inp, notF),
+// where notF is the boolean inverse of f.
+func FirstUntil[T any](inp iter.Seq[T], f func(T) bool) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for val := range inp {
+			if f(val) {
+				return
+			}
+			if !yield(val) {
+				return
+			}
+		}
+	}
+}
+
+// FirstWhile copies the input iterator to the output
+// while f returns true for the elements, then stops.
+// It is the same as FirstUntil(inp, notF),
+// where notF is the boolean inverse of f.
+func FirstWhile[T any](inp iter.Seq[T], f func(T) bool) iter.Seq[T] {
+	return FirstUntil(inp, func(t T) bool { return !f(t) })
+}
+
 // First returns the first value of seq and true.
 // If seq is empty, it returns the zero value of T and false.
 //
--- first_test.go
+++ first_test.go
@@ -1,6 +1,10 @@
 package seqs
 
-import "testing"
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
 
 func TestFirst(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
@@ -18,4 +22,35 @@ func TestFirst(t *testing.T) {
 			t.Errorf("got %v, want 17", got)
 		}
 	})
+}
+
+func TestFirstUntil(t *testing.T) {
+	cases := []struct {
+		inp, want []int
+	}{{
+		inp:  nil,
+		want: nil,
+	}, {
+		inp:  []int{4},
+		want: []int{4},
+	}, {
+		inp:  []int{4, 5, 6, 7},
+		want: []int{4, 5, 6, 7},
+	}, {
+		inp:  []int{4, 5, 6, 7, 8},
+		want: []int{4, 5, 6, 7},
+	}, {
+		inp:  []int{8, 9, 10},
+		want: nil,
+	}}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("case_%02d", i+1), func(t *testing.T) {
+			gotSeq := FirstUntil(slices.Values(tc.inp), func(x int) bool { return x > 7 })
+			gotSlice := slices.Collect(gotSeq)
+			if !slices.Equal(gotSlice, tc.want) {
+				t.Errorf("got %v, want %v", gotSlice, tc.want)
+			}
+		})
+	}
 }
