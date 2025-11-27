4224fe32b19c426e832369c1e8ec353f5cba5af7
Bob Glickstein <bobg@emphatic.com>
2025-11-27 10:51:55 -0800

    Add FromPages.

--- pages.go
+++ pages.go
@@ -1,6 +1,9 @@
 package seqs
 
-import "iter"
+import (
+	"fmt"
+	"iter"
+)
 
 // Pages converts an iterator of items into an iterator of pages of items.
 // Each page is a slice of up to pageSize items.
@@ -20,6 +23,25 @@ func Pages[T any](inp iter.Seq[T], pageSize int) iter.Seq[[]T] {
 
 		if len(page) > 0 {
 			yield(page)
+		}
+	}
+}
+
+// FromPages produces an iterator of items from a function that returns items a page at a time.
+// The boolean result from nextpage indicates whether there are more pages to come.
+func FromPages[T any](nextpage func() ([]T, bool)) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for {
+			page, ok := nextpage()
+			fmt.Printf("xxx got a page and %v\n", ok)
+			if !ok {
+				return
+			}
+			for _, item := range page {
+				if !yield(item) {
+					return
+				}
+			}
 		}
 	}
 }
